ecf20625d9705ae14ae31e17f3ad0ecabdca538f
Subham <120892516+subhamsubedi1943@users.noreply.github.com>
2025-04-04 11:09:03 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -4,8 +4,9 @@ public class Sum {
     public static void main(String[] args) {
         int num1=5;
         int num2=3;
+        int sum= num1+num2;
 
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum );
 
         
     }
