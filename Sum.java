59912a5bd4b21039a87f962f76e0cea966103b53
Subham <120892516+subhamsubedi1943@users.noreply.github.com>
2025-04-04 11:05:55 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,15 +2,8 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1 + num2;
+        int num1=5;
+        int num2=3;
 
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
 
